3e7c819e21b3a57f3f47ef34b958a514a16de78c
Skaylic <skaylic@yandex.ru>
2024-10-30 17:38:49 +0900

    Updating files
    Signed-off-by: Skaylic <skaylic@yandex.ru>

--- skay/Okx.py
+++ skay/Okx.py
@@ -121,8 +121,6 @@ class Okx:
 
     async def send_ticker(self, sz, side='buy', tag=''):
         tgtCcy = 'base_ccy'
-        if side == "sell":
-            tgtCcy = 'quote_ccy'
         if not tag:
             tag = 'bot'
         await self.send(self.ws, "order",
